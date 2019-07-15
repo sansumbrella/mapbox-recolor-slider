bd6dc2b4bda810f827a1d8040d5e1b6c0022dfec
David Wicks <david.wicks@mapbox.com>
2019-07-15 12:53:24 -0400

    Updates

---
+++ precache-manifest.de908e814c4e5bab2f85fbc0fca7d9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f189adef37555b6bdd2c37c0488d5948",
+    "url": "/mapbox-recolor-slider/index.html"
+  },
+  {
+    "revision": "ac2255ba1abe83443131",
+    "url": "/mapbox-recolor-slider/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "f53dc90612e86e30603c",
+    "url": "/mapbox-recolor-slider/static/css/main.3e47692b.chunk.css"
+  },
+  {
+    "revision": "ac2255ba1abe83443131",
+    "url": "/mapbox-recolor-slider/static/js/2.c245248d.chunk.js"
+  },
+  {
+    "revision": "f53dc90612e86e30603c",
+    "url": "/mapbox-recolor-slider/static/js/main.439f1896.chunk.js"
+  },
+  {
+    "revision": "81209516ea9b491b91ab",
+    "url": "/mapbox-recolor-slider/static/js/runtime~main.03a46469.js"
+  }
+]);
